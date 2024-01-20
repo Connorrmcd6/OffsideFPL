ff6a75745c7f63e01d2fb6c91bcbeb35a8da5f41
Connor McDonald <connormcd98@gmail.com>
2024-01-20 18:55:26 +0200

    added home module

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './feature/auth/auth.module';
+import { HomeModule } from './feature/home/home.module';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { AuthModule } from './feature/auth/auth.module';
     BrowserAnimationsModule,
     CoreModule,
     AuthModule,
+    HomeModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
