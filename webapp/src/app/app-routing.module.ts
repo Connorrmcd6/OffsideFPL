08c2bd86f7008a70bf988d49e53745c4323c6780
Connor McDonald <connormcd98@gmail.com>
2024-01-21 06:49:57 +0200

    added routing

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LandingComponent } from './core/landing/landing.component';
 import { LoginComponent } from './feature/auth/login/login.component';
 import { RegisterComponent } from './feature/auth/register/register.component';
 import { ForgotPasswordComponent } from './feature/auth/forgot-password/forgot-password.component';
+import { HomeDashboardComponent } from './feature/home/home-dashboard/home-dashboard.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent }
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'home', component: HomeDashboardComponent }
 ];
 
 @NgModule({
