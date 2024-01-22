e1fabf9d68dd2c820502394081b57978af849928
Connor McDonald <connormcd98@gmail.com>
2024-01-22 07:11:02 +0200

    profile component

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { LoginComponent } from './feature/auth/login/login.component';
 import { RegisterComponent } from './feature/auth/register/register.component';
 import { ForgotPasswordComponent } from './feature/auth/forgot-password/forgot-password.component';
 import { HomeDashboardComponent } from './feature/home/home-dashboard/home-dashboard.component';
+import { ProfileComponent } from './feature/home/profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
-  { path: 'home', component: HomeDashboardComponent }
+  { path: 'home', component: HomeDashboardComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
--- webapp/src/app/feature/home/home.module.ts
+++ webapp/src/app/feature/home/home.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeDashboardComponent } from './home-dashboard/home-dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    HomeDashboardComponent
+    HomeDashboardComponent,
+    ProfileComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    RouterModule
   ]
 })
 export class HomeModule { }
