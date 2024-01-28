8c8b7427c097fa173b098c126eebf01d0075b2de
Connor McDonald <connormcd98@gmail.com>
2024-01-28 08:31:34 +0200

    [added] login functionality

--- webapp/src/app/feature/auth/login/login.component.ts
+++ webapp/src/app/feature/auth/login/login.component.ts
@@ -1,22 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   hide = true;
 
   constructor(
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    public authService: AuthService
   ) {}
 
   goBack(): void {
     this.location.back();
   }
+
+  ngOnInit() { }
 }
