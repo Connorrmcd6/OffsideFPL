05cc6f7ceb7851d227963de77d83486475fa6600
Connor McDonald <connormcd98@gmail.com>
2024-02-02 07:38:35 +0200

    [Update] landing page

--- webapp/src/app/feature/screens/screens.module.ts
+++ webapp/src/app/feature/screens/screens.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {MatButtonModule} from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LandingComponent,
+    HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    RouterModule
   ]
 })
 export class ScreensModule { }
