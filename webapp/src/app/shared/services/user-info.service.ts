73bdb640d13fb24a9d0d380aa62cbc6c00fac713
Connor McDonald <connormcd98@gmail.com>
2024-02-11 09:35:01 +0200

    user info service

--- webapp/src/app/shared/services/user-info.service.ts
+++ webapp/src/app/shared/services/user-info.service.ts
@@ -1,9 +1,46 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { UserInfo } from './user-info';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserInfoService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient, // Inject HttpClient service
+    public afs: AngularFirestore, // Inject Firestore service
+    public ngZone: NgZone // NgZone service to remove outside scope warning
+  ) { }
+
+  getUserInfo() {
+    let placeholderTeamID: string = 'Not set';
+    let placeholderManagerName: string = 'Not set';
+    let placeholderTeamName: string = 'Not set';
+
+    const userInfo = JSON.parse(localStorage.getItem('userInfo') || '{}');
+
+    if (userInfo.teamID == null) {
+      return [placeholderTeamID, placeholderManagerName, placeholderTeamName];
+    }
+
+    return userInfo.teamID, userInfo.managerName, userInfo.teamName;  
+  }
+
+
+  fetchUserInfo(teamID: string) {
+    let baseURL: string = `https://fantasy.premierleague.com/api/entry/${teamID}/`;
+
+    return this.http.get(baseURL).pipe(
+      map((response: any) => {
+        const { player_first_name, player_last_name, name, player_region_name } = response;
+        const manager_name = `${player_first_name} ${player_last_name}`;
+        console.log(manager_name, name, player_region_name);
+        return { manager_name, name, player_region_name };
+      })
+    );
+  }
+
 }
