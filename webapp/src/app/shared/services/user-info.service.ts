7c9d84e34ab980db108f0342fcb3a7a08386060e
Connor McDonald <connormcd98@gmail.com>
2024-02-11 11:09:55 +0200

    team confirmation pop up

--- webapp/src/app/shared/services/user-info.service.ts
+++ webapp/src/app/shared/services/user-info.service.ts
@@ -15,29 +15,15 @@ export class UserInfoService {
     public ngZone: NgZone // NgZone service to remove outside scope warning
   ) { }
 
-  getUserInfo() {
-    let placeholderTeamID: string = 'Not set';
-    let placeholderManagerName: string = 'Not set';
-    let placeholderTeamName: string = 'Not set';
 
-    const userInfo = JSON.parse(localStorage.getItem('userInfo') || '{}');
-
-    if (userInfo.teamID == null) {
-      return [placeholderTeamID, placeholderManagerName, placeholderTeamName];
-    }
-
-    return userInfo.teamID, userInfo.managerName, userInfo.teamName;  
-  }
-
-
-  fetchUserInfo(teamID: string) {
+  // a function that takes a team id from an input and then makes a request to the FPL API to get the user's name and region
+  getUserInfo(teamID: string) {
     let baseURL: string = `https://fantasy.premierleague.com/api/entry/${teamID}/`;
 
     return this.http.get(baseURL).pipe(
       map((response: any) => {
         const { player_first_name, player_last_name, name, player_region_name } = response;
         const manager_name = `${player_first_name} ${player_last_name}`;
-        console.log(manager_name, name, player_region_name);
         return { manager_name, name, player_region_name };
       })
     );
