f77e484d6f3b4dd5daae6985b1a01bc3969f2d01
Connor McDonald <connormcd98@gmail.com>
2024-02-23 19:14:29 +0200

    idea

--- lambda/test/cmd/main.go
+++ lambda/test/cmd/main.go
@@ -11,6 +11,7 @@ func main() {
 	lambda.Start(handler)
 }
 
+// this function will need to take another argument called context which will direct it on which funcion to call
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 
 	switch request.HTTPMethod {
