ff5bb3cf686d78ba71203267ea9ffcb554ad80b3
Connor McDonald <connormcd98@gmail.com>
2024-02-29 08:20:20 +0200

    new modal templates

---
+++ webapp/src/app/feature/modals/join-league/join-league.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-league',
+  standalone: true,
+  imports: [],
+  templateUrl: './join-league.component.html',
+  styleUrl: './join-league.component.scss'
+})
+export class JoinLeagueComponent {
+
+}
