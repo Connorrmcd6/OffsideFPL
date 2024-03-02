ec982f7acc47c8842a802955a70a7b6f314b84dd
Connor McDonald <connormcd98@gmail.com>
2024-03-02 06:24:19 +0200

    added create league dialog

--- webapp/src/app/feature/modals/create-league/create-league.component.ts
+++ webapp/src/app/feature/modals/create-league/create-league.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-create-league',
   templateUrl: './create-league.component.html',
   styleUrl: './create-league.component.scss'
 })
-export class CreateLeagueComponent {
+export class CreateLeagueComponent implements OnInit {
+  leagueForm = this.fb.group({
+    leagueName: ['', Validators.required],
+    gameMode: ['', Validators.required]
+  });
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
 }
--- webapp/src/app/feature/modals/modals.module.ts
+++ webapp/src/app/feature/modals/modals.module.ts
@@ -7,6 +7,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { FplDeatilsInputDialogComponent } from './fpl-deatils-input-dialog/fpl-deatils-input-dialog.component';
 import { InstallPwaAlertComponent } from './install-pwa-alert/install-pwa-alert.component';
@@ -26,7 +28,9 @@ import { JoinLeagueComponent } from './join-league/join-league.component';
     MatIconModule,
     MatButtonModule,
     MatExpansionModule,
-    MatDialogModule
+    MatDialogModule,
+    MatSelectModule,
+    ReactiveFormsModule
   ]
 })
 export class ModalsModule { }
