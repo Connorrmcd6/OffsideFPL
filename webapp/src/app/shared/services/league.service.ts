2aa11ae31ed6f85b9ff1c7bf0ffd465801805d52
Connor McDonald <connormcd98@gmail.com>
2024-03-05 07:42:55 +0200

    league codes

--- webapp/src/app/shared/services/league.service.ts
+++ webapp/src/app/shared/services/league.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NgZone } from '@angular/core';
 import { UserInfo } from './user-info';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
-import { generate } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,14 +14,28 @@ export class LeagueService {
     public ngZone: NgZone // NgZone service to remove outside scope warning
   ) { }
 
-  generateLeague(leagueName: string, leagueMode: string, leaguePrivacy: string) {
+  async generateLeague(leagueName: string, leagueMode: string, leaguePrivacy: string) {
     let data: any = {}; // Declare and initialize data as an empty object
     data.adminID = this.userInfoData?.uid; // Add null check
     data.leagueName = leagueName;
     data.leagueMode = leagueMode;
     data.leaguePrivacy = leaguePrivacy;
 
-    return this.afs.collection('leagues').add(data)
+    // Generate a 6 character alphanumeric ID
+    let id = this.generateId(6);
+
+    // Check if a document with the same ID already exists
+    let doc = await firstValueFrom(this.afs.collection('leagues').doc(id).get());
+    while (doc?.exists) {
+      // If it does, generate a new ID
+      id = this.generateId(7);
+      doc = await firstValueFrom(this.afs.collection('leagues').doc(id).get());
+    }
+
+    // Set the unique ID
+    data.id = id;
+
+    return this.afs.collection('leagues').doc(id).set(data)
       .then(() => {
         console.log('League data added successfully');
       })
@@ -30,6 +44,16 @@ export class LeagueService {
         throw error;
       });
   }
+
+  generateId(length: number) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+  }
+
 }
 
 
