94e7e79c08f8eaea2b09ef453fb89159548d8de4
Connor McDonald <connormcd98@gmail.com>
2024-03-06 07:17:02 +0200

    adding user-leagues function

--- webapp/src/app/shared/services/league.service.ts
+++ webapp/src/app/shared/services/league.service.ts
@@ -28,7 +28,7 @@ export class LeagueService {
     let doc = await firstValueFrom(this.afs.collection('leagues').doc(id).get());
     while (doc?.exists) {
       // If it does, generate a new ID
-      id = this.generateId(7);
+      id = this.generateId(6);
       doc = await firstValueFrom(this.afs.collection('leagues').doc(id).get());
     }
 
@@ -38,6 +38,8 @@ export class LeagueService {
     return this.afs.collection('leagues').doc(id).set(data)
       .then(() => {
         console.log('League data added successfully');
+        // After the league is created, add a record to the user_leagues table
+        return this.addUserLeague(this.userInfoData?.uid, id);
       })
       .catch(error => {
         console.error('Error adding league data: ', error);
@@ -45,8 +47,28 @@ export class LeagueService {
       });
   }
 
+  async addUserLeague(userId: string | undefined, leagueId: string) {
+    if (!userId) {
+      throw new Error('User ID is undefined');
+    }
+
+    const userLeagueData = {
+      uid: userId,
+      league_id: leagueId
+    };
+
+    return this.afs.collection('user-leagues').add(userLeagueData)
+      .then(() => {
+        console.log('user-league data added successfully');
+      })
+      .catch(error => {
+        console.error('Error adding user-league data: ', error);
+        throw error;
+      });
+  }
+
   generateId(length: number) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let result = '';
     for (let i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * characters.length));
